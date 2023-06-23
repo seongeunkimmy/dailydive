db5a2567f14bb2e7f4370de09ca3511529b644b5
seongeunkimmy <kimusique7@gmail.com>
2023-06-23 11:28:17 +0900

    feat: home view added

---
+++ dailydive_project/dailydive_webapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views # 같은 폴더 내의 views.py를 import 
+
+app_name = 'dailydive_webapp'
+
+urlpatterns = [
+    path('', views.home_view, name='home_view'),
+]
--- dailydive_project/dailydive_webapp/views.py
+++ dailydive_project/dailydive_webapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def first_view(request):
+    return render(request, 'dailydive_webapp/home_view.html', {})
