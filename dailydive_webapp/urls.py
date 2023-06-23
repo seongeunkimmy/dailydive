fb78802773e5efdee1142f7adbf285dc78007fac
seongeunkimmy <kimusique7@gmail.com>
2023-06-23 17:59:40 +0900

    feat: bootstrap setup index html added

--- dailydive_webapp/urls.py
+++ dailydive_webapp/urls.py
@@ -4,5 +4,6 @@ from . import views # 같은 폴더 내의 views.py를 import
 app_name = 'dailydive_webapp'
 
 urlpatterns = [
-    path('', views.home_view, name='home_view'),
+    path('', views.index, name='index'),
+    path('home/', views.home_view, name='home_view'),
 ]
--- dailydive_webapp/views.py
+++ dailydive_webapp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    return render(request, 'dailydive_webapp/index.html', {})
+
 def home_view(request):
     return render(request, 'dailydive_webapp/home_view.html', {})
