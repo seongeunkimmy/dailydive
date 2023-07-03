2dc334978090b8ba1417bb541b3b2388054993ab
Jeongyoujin2 <welrodxv98@gmail.com>
2023-07-03 16:44:07 +0900

    feat:connect db

---
+++ bulk.py
@@ -0,0 +1,25 @@
+import os
+import django
+import csv
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailydive_project.settings')
+django.setup()
+
+from dailydive_webapp.models import solutions
+
+f = open(r"C:\Users\jyoo7\Downloads\dailydive_solution.csv", 'r', encoding='utf-8-sig')
+info = []
+
+rdr = csv.reader(f)
+for row in rdr:
+    sentiment, solution_title, solution_guide, img_url = row
+    tuple = (sentiment, solution_title, solution_guide, img_url)
+    info.append(tuple)
+f.close()
+
+instances = []
+for (sentiment, solution_title, solution_guide, img_url) in info:
+    instances.append(solutions(sentiment=sentiment, solution_title=solution_title,
+                              solution_guide=solution_guide,  img_url= img_url ))
+
+solutions.objects.bulk_create(instances)
